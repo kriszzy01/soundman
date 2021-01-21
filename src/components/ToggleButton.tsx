2ad251ec2a49dd7a80aacefe70d708800ce29f44
Chris <christianotu.d@gmail.com>
2021-01-21 20:59:34 +0100

    Added toggle button

---
+++ src/components/ToggleButton.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { ReactComponent as Sun } from "../assets/sun.svg";
+import { ReactComponent as Moon } from "../assets/moon.svg";
+
+export const ToggleButton: React.FC = () => {
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    if (toggle) {
+      document.querySelector("body")?.setAttribute("data-theme", "dark");
+    }
+
+    return () => {
+      document.querySelector("body")?.removeAttribute("data-theme");
+    };
+  }, [toggle]);
+
+  return (
+    <button
+      type="button"
+      data-type="toggle"
+      role="switch"
+      aria-label="dark mode"
+      aria-pressed={toggle}
+      aria-checked={toggle}
+      onClick={() => setToggle((toggle) => !toggle)}
+    >
+      <span>
+        <Sun />
+      </span>
+      <div className="toggle__thumb" data-toggle={toggle}></div>
+      <span>
+        <Moon />
+      </span>
+    </button>
+  );
+};
