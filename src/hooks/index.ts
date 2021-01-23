fdb747448d6c48f7fb8de3fbc73d50f992a36595
Chris <christianotu.d@gmail.com>
2021-01-23 17:50:01 +0100

    Added support for user system darkmode settings

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,6 +2,12 @@ import { useState, useEffect } from "react";
 
 export const useDarkMode = (initialValue: boolean) => {
   const [value, setValue] = useState(() => {
+    const userMode = window.matchMedia("(prefers-color-scheme:dark)").matches; //If user has darkmode set on system
+
+    if (userMode) {
+      return true;
+    }
+
     let darkMode = window.localStorage.getItem("darkMode");
 
     if (darkMode !== null) {
